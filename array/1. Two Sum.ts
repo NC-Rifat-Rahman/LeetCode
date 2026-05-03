5538e2ae43def07f4dda0db71f30cafa20fe07ef
NC-Rifat-Rahman <rahmanrifat61@gmail.com>
2026-05-03 17:00:01 +0600

    Array-1. Two Sum O(n^2) time complexity solution

---
+++ array/1. Two Sum.ts
@@ -0,0 +1,15 @@
+// O(n^2)
+function twoSum(nums: number[], target: number): number[] {
+    let ans: number[] = [];
+
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                ans[0] = i;
+                ans[1] = j;
+                break;
+            }
+        }
+    }
+    return ans;
+};
